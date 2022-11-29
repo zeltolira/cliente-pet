c2728c9a43f0aad0291c84957e05383b034e171c
ZELTO LIRA <you@example.com>
2022-11-29 20:55:25 -0300

    implementa ClienteApplicationService e novo Constructor cliente

--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteAplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteAplicationService.java
@@ -4,17 +4,25 @@ import org.springframework.stereotype.Service;
 
 import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
 import br.com.petz.clientepet.cliente.application.api.ClienteResquest;
+import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
+import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteAplicationService implements ClienteService {
-
+	private final ClienteRepository clienteRepository;
+	
 	@Override
 	public ClienteResponse criaCliente(ClienteResquest clienteRequest) {
 		log.info("[inicia] ClienteApplicationService = criaCliente");
+		Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
 		log.info("[finaliza] ClienteApplicationService = criaCliente");
-		return null;
+		return ClienteResponse.builder()
+				.idCliente(cliente.getIdCliente())
+				.build();
 	}
 
 }
