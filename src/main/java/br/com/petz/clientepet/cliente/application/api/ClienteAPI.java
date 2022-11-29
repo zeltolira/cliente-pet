af0bd41a595153fcfa9a4afc36749da573e16871
ZELTO LIRA <you@example.com>
2022-11-29 05:13:58 -0300

    implementa contrato da API

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
-	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	
 	ClienteResponse postCliente(@RequestBody ClienteResquest clienteRequest);
 	
 }
