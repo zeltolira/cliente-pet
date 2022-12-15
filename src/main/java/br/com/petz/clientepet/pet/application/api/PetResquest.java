93606ab07051731bced9eadc274768015b21ed27
ZELTO LIRA <you@example.com>
2022-12-14 21:17:13 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetResquest.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetResquest.java
@@ -1,5 +1,29 @@
 package br.com.petz.clientepet.pet.application.api;
 
-public class PetResquest {
+import java.time.LocalDate;
 
+import br.com.petz.clientepet.pet.domain.Porte;
+import br.com.petz.clientepet.pet.domain.SexoPet;
+import br.com.petz.clientepet.pet.domain.TipoPet;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class PetResquest {
+	@NotBlank
+	private String nomePet;
+	private Porte porte;
+	@NotNull
+	private TipoPet tipo;
+	private String microchip;
+	@NotBlank
+	private String raca;
+	@NotNull
+	private SexoPet sexo;
+	private String pelagemCor;
+	@NotNull
+	private LocalDate dataNascimento;
+	private String rga;
+	private Integer peso;
 }
